1574cbeec38bf178e6563ed70b3930c96afb4af7
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-05-07 15:37:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os
 
-from neptune_optuna._version import get_versions
 from setuptools import setup
+
+from neptune_optuna._version import get_versions
+
 
 def main():
     with open('README.md') as readme_file:
@@ -36,11 +38,33 @@ def main():
         url="https://github.com/neptune-ai/neptune-optuna",
         long_description=readme,
         long_description_content_type="text/markdown",
-        license='MIT License',
+        license='Apache License 2.0',
         install_requires=base_libs,
         extras_require=extras,
         packages=['neptune_optuna', 'neptune_optuna.impl'],
-        zip_safe=False
+        zip_safe=False,
+        classifiers=[
+            # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License',
+            'Natural Language :: English',
+            'Operating System :: MacOS',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        ],
+        keywords=['MLOps', 'ML Experiment Tracking', 'ML Model Registry', 'ML Model Store', 'ML Metadata Store'],
     )
 
 
